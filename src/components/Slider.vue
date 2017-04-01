78c7744a877c41a88241d3264a86ac490dcf420b
Chobits <chobits@live.cn>
2017-04-01 11:43:58 +0800

    1

---
+++ src/components/Slider.vue
@@ -0,0 +1,68 @@
+﻿<template>
+    <div class="mui-slider">
+        <div class="mui-slider-group mui-slider-loop">
+            <div class="mui-slider-item mui-slider-item-duplicate">
+                <a :href="last.link">
+                    <img :src="last.img" />
+                </a>
+            </div>
+            <div class="mui-slider-item" v-for="item in items">
+                <a :href="item.link">
+                    <img :src="item.img" />
+                </a>
+            </div>
+            <div class="mui-slider-item mui-slider-item-duplicate">
+                <a :href="first.link">
+                    <img :src="first.img" />
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    const mui = require('mui')
+
+    module.exports = {
+        name: 'MuiSlider',
+        props: {
+            images: {
+                type: Array,
+                default: []
+            },
+            interval: {
+                type: Number,
+                default: 0
+            }
+        },
+        mounted() {
+            this.$nextTick(() => {
+                //获得slider插件对象
+                var gallery = mui(this.$el);
+                gallery.slider({
+                    interval: this.interval//自动轮播周期，若为0则不自动播放，默认为0；
+                });
+            })
+        },
+        computed: {
+            items() {
+                let arr = [];
+
+                arr = this.images.map(t => {
+                    return {
+                        img: t,
+                        link: '#'
+                    }
+                })
+
+                return arr;
+            },
+            first() {
+                return this.items[0]
+            },
+            last() {
+                return this.items[this.items.length - 1]
+            }
+        }
+    }
+</script>
--- src/views/slider.vue
+++ src/views/slider.vue
@@ -1,66 +1,23 @@
 ﻿<template>
     <div class="mui-content">
-        <div class="mui-content-padded">
-            <h5 class="mui-content-padded">默认</h5>
-            <div class="mui-numbox">
-                <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                <input class="mui-input-numbox" type="number">
-                <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-            </div>
-            <h5 class="mui-content-padded">限定最小值和最大值(1~9)</h5>
-            <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
-                <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                <input id="test" class="mui-input-numbox" type="number" value="5">
-                <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-            </div>
-            <h5 class="mui-content-padded">设定步长值(步长 10)</h5>
-            <div class="mui-numbox" data-numbox-step="10" data-numbox-min="10" data-numbox-max="90">
-                <button class="mui-btn mui-btn-numbox-minus" type="button" disabled="">-</button>
-                <input class="mui-input-numbox" type="number">
-                <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-            </div>
-            <h5 class="mui-content-padded">取值操作</h5>
-            <div class="mui-numbox">
-                <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                <input id="box" class="mui-input-numbox" type="number">
-                <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-            </div>
-            <button id="btn" class="mui-btn" style="padding: 7px 12px;">取当前值</button>
-            <h5 class="mui-content-padded">在行内</h5> 购买数量:
-            <div class="mui-numbox">
-                <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                <input class="mui-input-numbox" type="number">
-                <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-            </div>
-            <h5 class="mui-content-padded">在表单中</h5>
-            <div class="mui-card">
-                <form class="mui-input-group">
-                    <div class="mui-input-row">
-                        <label>数字框一</label>
-                        <div class="mui-numbox">
-                            <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                            <input class="mui-input-numbox" type="number">
-                            <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-                        </div>
-                    </div>
-                    <div class="mui-input-row">
-                        <label>数字框二</label>
-                        <div class="mui-numbox">
-                            <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                            <input class="mui-input-numbox" type="number">
-                            <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <h5 class="mui-content-padded">通过样式定义大小(220x60)</h5>
-            <div class="mui-numbox" style="width: 220px;height: 60px;">
-                <button class="mui-btn-numbox-minus" type="button">-</button>
-                <input class="mui-input-numbox" type="number">
-                <button class="mui-btn-numbox-plus" type="button">+</button>
-            </div>
-            <br>
-            <br>
-        </div>
+        <mui-slider :images="images" :interval="5000"></mui-slider>
     </div>
 </template>
+
+<script>
+    module.exports = {
+        data() {
+            return {
+                images: []
+            }
+        },
+        created() {
+            this.images.push('http://www.dcloud.io/hellomui/images/yuantiao.jpg');
+            this.images.push('http://www.dcloud.io/hellomui/images/shuijiao.jpg');
+            this.images.push('http://www.dcloud.io/hellomui/images/muwu.jpg');
+        },
+        components: {
+            MuiSlider: require('components/Slider')
+        }
+    }
+</script>
